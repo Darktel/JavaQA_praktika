b90938088472557d740163dbd980acf384a2b163
gusev_ae <gusev_ae@magnit.ru>
2025-05-30 00:53:04 +0300

    Удалил оставшиеся комментарии. И 1 лишний импорт. Изменил цикл.

--- src/main/java/model/Meat.java
+++ src/main/java/model/Meat.java
@@ -1,10 +1,5 @@
 package model;
 
-
-/*
-Класс Meat — для мяса. Это класс с конструктором. Конструктор принимает два входных параметра — количество и цену.
-
- */
 public class Meat extends Food {
     public Meat(int amount, double price) {
         super(amount, price, false);
--- src/main/java/model/service/ShoppingCart.java
+++ src/main/java/model/service/ShoppingCart.java
@@ -1,16 +1,6 @@
 package model.service;
 
-/*
-В нём создай класс для корзины — ShoppingCart. Добавь в него поле — массив элементов из объектов типа Food.
-Понадобится конструктор с одним параметром — массивом элементов Food. С его помощью ты создашь объект корзины.
-В классе ShoppingCart реализуй методы для работы с корзиной:
-получить общую сумму товаров в корзине без скидки,
-получить общую сумму товаров в корзине со скидкой,
-получить общую сумму всех вегетарианских продуктов в корзине без скидки.
-*/
-
 import model.Food;
-import model.constants.Discount;
 
 public class ShoppingCart {
     private Food[] food;
@@ -20,9 +10,9 @@ public class ShoppingCart {
 
     public double getSumWithoutDiscount() {
         double sum = 0;
-            for (int i = 0; i < food.length; i++) {
-                sum += food[i].getTotalPrice();
-            }
+        for (Food value : food) {
+            sum += value.getTotalPrice();
+        }
         return sum;
     }
 
